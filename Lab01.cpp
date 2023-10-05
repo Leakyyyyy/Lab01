b8eb10fc4d3b2b2804f64978efbe02d281fead5b
Leakyyyyy <145537370+Leakyyyyy@users.noreply.github.com>
2023-10-05 17:17:03 +0700

    Update Lab01.cpp
    Поправил

--- Lab01.cpp
+++ Lab01.cpp
@@ -1,19 +1,29 @@
+/*******************************************************
+* Степанов Артём Андреевич,ПИ-231
+* Share-Link:https://https://onlinegdb.com/CzkaXUPZp
+* Тема: Программирование линейных алгоритмов, вариант 1                             
+********************************************************/
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-int main()
-{ 
-    double dlin = 0.994;
-    int og = 50;
-    double pi = 3.14;
-    double g = 9.8;
-    double t1 = 2 * pi * sqrt(dlin/g);
-    double t2 = 2 * pi * sqrt((dlin/g)*(1+(1/16)*pow(og,2.0)));
-    double t2 = 2 * pi * sqrt((dlin/g)*(1+pow(((1/4)sinf),2.0)*pow(og,2.0)));
-    cout << t1 << endl;
-    cout << t2 << endl;
+int main() { 
+double dlin, og, pi, g;
     
+    cout << "Введите значение длинны: ";
+    cin >> dlin;
+    
+    cout << "Введите значение угла наклона: ";
+    cin >> og;
+    
+    cout << "Введите значение π: ";
+    cin >> pi;
+    
+    cout << "Введите значение ускорения свободного падения(g): ";
+    cin >> g;
+    cout << "t1 = " << 2 * pi * sqrt(dlin / g) << endl;
+    cout << "t2 = " << 2 * pi * sqrt((dlin / g) * (1 + (1 / 16) * pow(og, 2.0))) << endl;
+    cout << "t3 = " << 2 * pi * sqrt((dlin / g) * (1 + pow(((1 / 4) * sin(pi)), 2.0) * pow(og, 2.0))) << endl;
     
 }
