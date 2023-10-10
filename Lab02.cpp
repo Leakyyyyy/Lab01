47439d5b7d83e1087d4eaf995d0a5ea9bb9e6bc5
Leakyyyyy <145537370+Leakyyyyy@users.noreply.github.com>
2023-10-10 16:56:58 +0700

    Update Lab02.cpp

--- Lab02.cpp
+++ Lab02.cpp
@@ -1,21 +1,27 @@
-/******************************************************************************
-this is variant 20
-*******************************************************************************/
+/*******************************************************
+* 
+* Степанов Артём Андреевич,ПИ-231
+* Share-Link:https://onlinegdb.com/dfybpmZZl
+* Тема: Циклы с пред- и постусловием, вариант 20
+* 
+********************************************************/
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-int main()
-{
-   auto M = 500;
-   auto q = 90;
-   auto u = 18;
-   auto e = 2.718;
-   int toki[10] {0, 2, 4, 6, 8, 10, 15, 20, 25, 30};
-   for(double t : toki){
-        auto v = u*(1-pow(e, -((q*t)/M)));
-        auto n = 2*(1-(v/u))*(v/u);
+int main() {
+   double M = 500.0;
+   double q = 90.0;
+   double u = 18.0;
+   double e = 2.718;
+   double toki[10] {0, 2.0, 4.0, 6.0, 8.0, 10.0, 15.0, 20.0, 25.0, 30.0};
+   for(double t : toki) {
+        double v;
+        v = u * (1 - pow(e, -( (q * t) / M) ) );
+        double n;
+        n = 2 * ( 1 - (v / u) ) * (v / u);
         cout << "t=" << t << endl;
         cout << "v=" << v << endl;
         cout << "n=" << n << endl;
